faaca2554185083aedce1ad24ceaf7818320d096
Susiepolly <susie_poll@yahoo.co.uk>
2020-08-04 17:49:44 +0100

    database trial

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-shallow-48285',
     user : 'postgres',
-    password : 'Database13',
+    password : '',
     database : 'smart_brain'
   }
 });
